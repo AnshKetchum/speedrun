6d47540e28349fa759a0f7b017d5c942882b4f63
Ansh <eye.am.ansh@gmail.com>
2026-03-27 17:52:45 -0700

    push run config

--- run.sh
+++ run.sh
@@ -5,19 +5,21 @@
 
 # --ut_n_blocks = number of blocks 
 # --ut_max_steps = loops per block
+mkdir -p logs
 accelerate launch alg/run.py \
     --model_type universal_transformer \
     --ut_tokenizer gpt2 \
     --ut_d_model 128 \
     --ut_n_heads 4 \
     --ut_d_ff 256 \
-    --ut_max_steps 6 \
-    --ut_n_blocks 1 \
+    --ut_max_steps 2 \
+    --ut_n_blocks 3 \
     --ut_eps 0.01 \
     --ut_tau 0.01 \
     --ut_dropout 0.1 \
     --no_distil \
     --max_steps 1000 \
     --save_steps 150 \
-    --output_dir ut_model \
-    --run_name universal_transformer
+    --output_dir checkpoints/ut_model_3_block_2_loop \
+    --run_name universal_transformer \
+    2>&1 | tee logs/run_$(date +%Y%m%d_%H%M%S).log
